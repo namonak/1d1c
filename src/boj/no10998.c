a7989373f6b9d96da069241c4680703b2abd58af
namonak <namonak@gmail.com>
2023-06-05 13:40:43 +0900

    build: modify code and CMakeLists to prioritize the execution of test code

--- src/boj/no10998.c
+++ src/boj/no10998.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int multiply(int a, int b) {
+int solve_no10998(int a, int b) {
     return a * b;
 }
 
+#ifndef TEST
+#ifdef NO10998
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%d", multiply(a, b));
+    printf("%d", solve_no10998(a, b));
     return 0;
 }
+#endif
+#endif
--- test/boj/no10998.test.c
+++ test/boj/no10998.test.c
@@ -1,19 +1,23 @@
 #include "unity.h"
+#include "boj/test_cases.h"
+
+#ifdef TEST
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_no10998);
+    UNITY_END();
+    return 0;
+}
+
+void test_no10998(void) {
+    int a = 1;
+    int b = 2;
+    int expected = 2;
+    int result = solve_no10998(a, b);
+    TEST_ASSERT_EQUAL_INT(expected, result);
+}
 
 void setUp(void) {}
 
 void tearDown(void) {}
-
-void test_multiply(void) {
-    int a = 1;
-    int b = 2;
-    int expected = 2;
-    int result = multiply(a, b);
-    TEST_ASSERT_EQUAL_INT(expected, result);
-}
-
-int main(void) {
-    UNITY_BEGIN();
-    RUN_TEST(test_multiply);
-    return UNITY_END();
-}
+#endif
